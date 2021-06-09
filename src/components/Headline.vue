93f3c5d0797cb8906cbf942e1d7c9a5228234273
YongheeAn <ayh0429@gmail.com>
2021-06-09 09:41:22 +0900

    header

---
+++ src/components/Headline.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+      <h1>
+    <span>OMDb API</span>
+    <div class="uppercase">The Open<br />
+    Movie Database</div>
+  </h1>
+  <p>
+    The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are contributed and maintained by our users.
+    If you find this service useful, please consider making a one-time donation or become a patron.
+  </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.container {
+  padding-top: 2.5em;
+}
+h1 {
+  line-height: 1;
+  font-family: "Oswald" , sans-serif;
+  font-size: 80px;
+  font-weight:700;
+  span {
+    color: $primary;
+  }
+  .uppercase {
+    text-transform: uppercase;
+  }
+}
+
+p {
+  margin: 30px 0;
+  color: $gray-600;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <RouterLink
+  to="/"
+  class="logo">
+  <span>OMDbAPI</span>.com
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+  .logo {
+    font-family: "Oswald", sans-serif;
+    font-size: 20px;
+    color: $black;
+    text-decoration: none;
+    font-weight:700;
+    &:hover {
+      color: $black;
+    }
+    span {
+      color: $primary;
+    }
+  }
+</style>
